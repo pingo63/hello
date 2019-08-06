8d1a46645aa015961d0b8bb07c7e3bae02278e7f
岳平 <pingo63@163.com>
2019-08-06 08:28:41 +0800

    hello工程修改

---
+++ src/main/java/com/test/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("greeting","Hello Spring MVC");
+        return "hello";
+    }
+}
